4fbaac7edf9c70f21b4eb19a3b37f9db56636eba
Rafael Pivatto <rafael.pivatto@gmail.com>
2020-07-21 23:39:53 -0300

    add test informations on choose server page

--- src/pages/ChooseServer/index.tsx
+++ src/pages/ChooseServer/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
 import { Container } from './styles';
+import { useAuth } from '../../hooks/auth';
 
 const ChooseServer: React.FC = () => {
+  const { user, guilds, signOut } = useAuth();
+
   return (
     <Container>
-      <p>Select Server</p>
+      <p>Olá, {user.name}</p>
+      <p>Escolha um dos servidores abaixo para gerenciar:,</p>
+      <br />
+      <br />
+      <ul>
+        {guilds.map((guild) => (
+          <li>
+            <img src={guild.icon} alt={guild.name} />
+            <p>{guild.name}</p>
+            <br />
+          </li>
+        ))}
+      </ul>
+      <br />
+      <br />
+      <br />
+      <button type="button" onClick={signOut}>
+        Sair
+      </button>
     </Container>
   );
 };
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import {
-  Container
-} from './styles';
+import { Container } from './styles';
 
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
   const { user } = useAuth();
-
 
   return (
     <Container>
